5e28d71834723e217858da47dbc1c7507587106d
d1joseph <dhiv.joseph@gmail.com>
2022-03-27 23:22:29 +1100

    cicd/heroku setup.

---
+++ client/src/components/creator/Creator.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './creator.css';
+
+
+const Creator = () => {
+    return (
+        <div className='colorBlue__featured-creators'>
+            <img className='creator-bg' src="" alt="" />
+            <div className='creator-avatar'>
+                <img src="" alt="" />
+            </div>
+            <div className='wrapper'>
+                <img className='creator-verification' src="" alt="" />
+                <h1 className='creator-username'></h1>
+            </div>
+            <h5 className='creator-handle'></h5>
+            <div></div>
+        </div>
+    )
+}
+
+export default Creator;
